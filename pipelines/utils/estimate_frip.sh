ff08976ef143c0e9c89d47c7bd65056968af22bd
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2016-11-02 13:58:37 -0500

    feat(pipelines): move to current structure

--- pipelines/utils/estimate_frip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-inBamBed=$1
-inIntersectBed=$2
-
-treads=`wc -l $inBamBed | awk '{print $1}'`
-fripEst=`cat $inIntersectBed | awk -v tot=$treads 'BEGIN{cnt=0}{cnt+=$NF}END{print cnt/tot}'`
-echo $fripEst
