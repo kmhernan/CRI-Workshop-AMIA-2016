bfc033c066ec84a34cd45350c50d4db8cd358e97
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2016-11-02 13:59:10 -0500

    feat(pipeline): updated pipeline

---
+++ workshop_extended/pipelines/RunAmiaPipeline.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# This script simply runs the BDS pipeline for the AMIA data.
+# This is only a small subset of the data so that it can run quickly.
+
+echo "---------------------------------------------------------------------"
+echo "2016 AMIA Pre-symposium Workshop"
+echo "Center for Research Informatics, University of Chicago"
+echo "Kyle Hernandez, Ph.D."
+echo ""
+echo "---------------------------------------------------------------------"
+echo "---------------------------------------------------------------------"
+echo "This script runs the ChIP-seq pipeline using BigDataScript and a small"
+echo "subset of the data."
+echo ""
+
+# Get the pipeline directory 
+repo=$(dirname $PWD)
+
+# make output directory if needed
+odir="${repo}/data/sample_run"
+if [ -d $odir ]; then rm -rf $odir; fi 
+mkdir -p $odir 
+echo "Top-level output directory of pipeline run: $odir"
+
+# Run/Log dir
+runDir="${odir}/run"
+logDir="${odir}/logs"
+echo "Analysis files output directory: $runDir"
+echo "Log files output directory: $logDir"
+
+# run 
+script="${repo}/AMIA-ChIP-Seq-Pipeline.bds"
+echo "Running the pipeline..."
+echo `date`
+
+cd $odir && /usr/bin/time -v -o 2016-AMIA-ChIPSeq-Run.time \
+bds $script -configFile ${repo}/config/chipseq.cfg \
+-contrastFiles ${repo}/config/contrast.Ab1.input.cfg \
+${repo}/config/contrast.Ab1.IgG.cfg \
+-runDir $runDir -logDir $logDir > 2016-AMIA-ChIPSeq-Run.logs 2>&1
+
+echo "Finished running..."
+echo `date`
---
+++ workshop_extended/pipelines/utils/run_multiqc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This bash script runs multiqc for the ChIP-seq dataset and outputs it into
+# the workshop_extended/pipeline/data directory of the repository.
+idir="/home/ubuntu/data/chipseq/fullset/fastqc"
+
+# Get the pipeline directory 
+repo=$(dirname $(dirname $PWD))
+odir="${repo}/data"
+
+# run
+multiqc="/home/ubuntu/anaconda2/envs/multiqc/bin/multiqc"
+cd $idir && $multiqc -d --title "ChIP Seq Dataset" -f -o $odir . 
